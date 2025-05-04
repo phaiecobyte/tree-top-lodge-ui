60534f00c4e92a8771af7090cf528e6d52da3514
phaiecobyte <brave6823@gmail.com>
2025-05-04 19:17:33 +0700

    fix active navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { FoodBeverageComponent } from './components/food-beverage/food-beverage.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { AccomodationComponent } from './components/accomodation/accomodation.component';
 
 export const routes: Routes = [
-    {path:'home',component:FoodBeverageComponent},
+    {path:'accommodation',component:AccomodationComponent},
     {path:'food-beverage',component:FoodBeverageComponent},
     {path:'product-detail',component:ProductDetailComponent},
-    {path:'',pathMatch:'full',redirectTo:'home'}
+    {path:'',pathMatch:'full',redirectTo:'accommodation'}
 ];
--- src/app/layout/navbar.component.ts
+++ src/app/layout/navbar.component.ts
@@ -4,25 +4,39 @@ import { RouterModule } from "@angular/router";
 
 @Component({
     selector: "app-navbar",
-    imports: [ThemeSwitcherComponent,RouterModule],
+    imports: [ThemeSwitcherComponent, RouterModule],
     template: `
         <nav class="navbar navbar-expand-lg bg-primary shadow-sm">
             <div class="container">
-                <a href="" class="navbar-brand d-none d-lg-inline-block">
+                <a routerLink="/" class="navbar-brand d-none d-lg-inline-block">
                     <img class="logo" src="./logo-Treetop-lodge.png" alt="Tree Top Lodge Logo">
                 </a>
                 <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">  
-                    <span class=" navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" routerLink="food-beverage" >Food & Beverage</a></li>  
-                        <li class="nav-item"><a class="nav-link" href="#">Attraction</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Service</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Clients</a></li>                        
-                        <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" routerLink="/accommodation" routerLinkActive="active">Accommodation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" routerLink="/food-beverage" routerLinkActive="active">Food & Beverage</a>
+                        </li>  
+                        <li class="nav-item">
+                            <a class="nav-link" routerLink="/attraction" routerLinkActive="active">Attraction</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" routerLink="/service" routerLinkActive="active">Service</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" routerLink="/clients" routerLinkActive="active">Clients</a>
+                        </li>                        
+                        <li class="nav-item">
+                            <a class="nav-link" routerLink="/about" routerLinkActive="active">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" routerLink="/contact" routerLinkActive="active">Contact Us</a>
+                        </li>
                     </ul>      
                 </div>
                 <app-theme-switcher></app-theme-switcher>              
@@ -41,11 +55,15 @@ import { RouterModule } from "@angular/router";
         .navbar-nav .nav-link {
             color: #fff !important;
         }
-        .navbar-nav .nav-link:hover {
+        .navbar-nav .nav-link:hover, 
+        .navbar-nav .nav-link.active {
             border-bottom: 2px solid #fff;
-            cursor:pointer;
+            cursor: pointer;
         }
-    `
+        .active {
+            font-weight: bold;
+        }
+        `
     ],
 })
 
