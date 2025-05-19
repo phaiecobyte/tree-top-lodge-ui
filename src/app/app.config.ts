33c8a9320e1cf4edffb126fc6f23abe3d3d14f64
phaiecobyte <brave6823@gmail.com>
2025-05-19 19:23:22 +0700

    refactor app configuration to include zone change detection and import providers for Firebase services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,15 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    importProvidersFrom([
+      provideFirebaseApp(() => firebaseApp),
+      provideFirestore(() => getFirestore(firebaseApp)),
+      provideStorage(() => getStorage(firebaseApp)),
+      provideAuth(() => getAuth(firebaseApp))
+    ])
+  ]
 };
